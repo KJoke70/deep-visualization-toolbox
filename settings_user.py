7ba29a514261d39c71003b3a12da368dd6035d73
Martin Lautenbacher <lautenbacher.martin@gmail.com>
2018-01-22 12:54:54 +0100

    settings for vgg and vgg_flickrlogos models

---
+++ settings_user.py
@@ -0,0 +1,14 @@
+
+##################################### user related settings #####################################
+
+# Use GPU? Default is True.
+caffevis_mode_gpu = False
+
+#caffevis_gpu_id = 0
+
+# network selection, for network settings see file settings_local.py
+#model_to_load = 'caffenet_yos' # AlexNet
+#model_to_load = 'bvlc_googlenet'
+#model_to_load = 'squeezenet'
+#model_to_load = 'vgg'
+model_to_load = 'vgg_flickrlogos'
