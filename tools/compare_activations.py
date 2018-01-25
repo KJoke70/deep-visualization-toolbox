12af1812bcd60bbaaed47a63f7aecce3a9751389
Martin <lautenbacher.martin@gmail.com>
2018-01-25 13:11:14 +0100

    removed multilayer, cleanup, improved count

--- tools/compare_activations.py
+++ tools/compare_activations.py
@@ -48,76 +48,52 @@ def main():
     save_execution_data(args, execution_time, os.path.join(args.outdir, 'execution_data.txt'))
     
     # Check if multiple layers need to be compared or just one
-    compare_all_layers = False
     
     for l in file1:
-        try:    
-            int(l)
-            top_n = len(file1[l])
-            break
-        except ValueError:
-            compare_all_layers = True
-            top_n = len(file1[l])
-            break
+        top_n = len(file1[l])
+        break
 
     top_n = args.N if (args.N <= top_n and args.N > 0) else top_n
     
-    generate_indices_plot(file1, file2, top_n, image_names, compare_all_layers, args.outdir)
+    generate_indices_plot(file1, file2, top_n, image_names, args.outdir)
 
 #TODO split into to functions -> extract_data and make_plot
-def generate_indices_plot(data1, data2, top_n, image_names, mult_layers, outdir): #TODO multilayer
+def generate_indices_plot(data1, data2, top_n, image_names, outdir):
     #overall percentage of equal indices in the top_n, with and without considering order
-    if mult_layers: 
-        nr_tops_ordered = dict()
-        nr_tops = dict()
-        indices_ordered = dict()
-        indices = dict()
+    nr_tops_ordered = dict()
+    nr_tops = dict()
+    indices_ordered = dict()
+    indices = dict()
 
-    else:
-        nr_tops_ordered = []
-        nr_tops = []
-        indices_ordered = []
-        indices = []
-        
     x_axis = np.arange(1, top_n + 1, 1)
-    for i in x_axis:
-        if mult_layers:
-            for l in data1:
-                nr_tops_ordered[l] = []
-                nr_tops[l] = []
-                indices_ordered[l] = []
-                indices[l] = []
-                
-                t_ord, idxs_ord = compare_index_single_layer(data1[l], data2[l], True, i, image_names)
-                t, idxs = compare_index_single_layer(data1[l], data2[l], False, i, image_names)
-                indices_ordered[l].append(idxs_ord)
-                indices[l].append(idxs)  
-                nr_tops_ordered[l].append(t_ord)
-                nr_tops[l].append(t)
-        else:
-            t_ord, idxs_ord = compare_index_single_layer(data1, data2, True, i, image_names)
-            t, idxs = compare_index_single_layer(data1, data2, False, i, image_names)
-            indices_ordered.append(idxs_ord)
-            indices.append(idxs)            
-            nr_tops_ordered.append(t_ord)
-            nr_tops.append(t)
+    for l in data1:
+        nr_tops_ordered[l] = []
+        nr_tops[l] = []
+        indices_ordered[l] = []
+        indices[l] = []
+        for n in x_axis:            
+            t_ord, idxs_ord = compare_index_single_layer(data1[l], data2[l], True, n, image_names)
+            t, idxs = compare_index_single_layer(data1[l], data2[l], False, n, image_names)
+            
+            indices_ordered[l].append(idxs_ord)
+            indices[l].append(idxs)  
+            nr_tops_ordered[l].append(t_ord)
+            nr_tops[l].append(t)
+            
 
     mkdir_p(outdir)
 
     # created axes and ylim for plot    
-    if mult_layers:
-        min_y = 1.0
-        max_y = 0.0
-        for l in nr_tops:
-            temp_min = min(nr_tops_ordered[l] + nr_tops[l])
-            temp_max = max(nr_tops_ordered[l] + nr_tops[l])
-            if temp_min < min_y:
-                min_y = temp_min
-            if temp_max > max_y:
-                max_y = temp_max
-    else:
-        min_y = min(nr_tops_ordered + nr_tops)
-        max_y = max(nr_tops_ordered + nr_tops)
+    min_y = 1.0
+    max_y = 0.0
+    for l in nr_tops:
+        temp_min = min(nr_tops_ordered[l] + nr_tops[l])
+        temp_max = max(nr_tops_ordered[l] + nr_tops[l])
+        if temp_min < min_y:
+            min_y = temp_min
+        if temp_max > max_y:
+            max_y = temp_max
+
     abs_diff = abs(max_y - min_y)
     min_y -= abs_diff / float(top_n)
     max_y += abs_diff / float(top_n)
@@ -139,30 +115,53 @@ def generate_indices_plot(data1, data2, top_n, image_names, mult_layers, outdir)
             ax.annotate("%.3f" % j,xy=(i,j))
         plt.savefig(filename, dpi=300)
 
-    if mult_layers:
-        for l in nr_tops_ordered:
-            plot_data(nr_tops_ordered[l], os.path.join(outdir, l + '_nr_tops_ordered.png'), 'Equal units with considering order\nlayer: ' + l)
-            plot_data(nr_tops[l], os.path.join(outdir, l + '_nr_tops.png'), 'Equal units without considering order\nlayer: ' + l)
-    else:
-        plot_data(nr_tops_ordered, os.path.join(outdir, 'nr_tops_ordered.png'), 'Equal units with considering order')
-        plot_data(nr_tops, os.path.join(outdir, 'nr_tops.png'), 'Equal units without considering order')
+    for l in nr_tops_ordered:
+        plot_data(nr_tops_ordered[l], os.path.join(outdir, l + '_nr_tops_ordered.png'), 'Equal units with considering order\nlayer: ' + l)
+        plot_data(nr_tops[l], os.path.join(outdir, l + '_nr_tops.png'), 'Equal units without considering order\nlayer: ' + l)
 
     # TODO no duplicates?
-    count_ordered = count_indices(indices_ordered)
-    count = count_indices(indices)
+    count1 = count_indices(data1, top_n)
+    count2 = count_indices(data2, top_n)
+    for l in count1:
+        for c in count1[l]:
+            print l, ':', c, ':', count1[l][c]
+    
+    print'-'*30
+    
+    for l in count2:
+        for c in count2[l]:
+            print l, ':', c, ':', count2[l][c]
+    #count_ordered = count_indices(indices_ordered)
+    #count = count_indices(indices)
 
 
-def count_indices(indices):
-    """ returns array with [{unit_idx : occurences}, ...] for [0;top_n[ """
-    count = []
-    for n in xrange(len(indices)):
-        count.append(dict())
-        for img_idx in xrange(len(indices[n])):
-            for key in indices[n][img_idx]:
-                if key in count:
-                    count[n][key] += 1
+#def count_indices(indices):
+#    """ returns array with [{unit_idx : occurences}, ...] for [0;top_n[ """
+#    count = []
+#    for n in xrange(len(indices)):
+#        count.append(dict())
+#        for img_idx in xrange(len(indices[n])):
+#            for key in indices[n][img_idx]:
+#                if key in count:
+#                    count[n][key] += 1
+#                else:
+#                    count[n][key] = 1
+#    return count
+def count_indices(data, top_n):
+    """ 
+    data: {layer : { img_idx : [(unit_idx, val), ... ]}}
+    returns: {layer : {unit_idx : count, ...}}
+    """
+    count = dict()
+    for layer in data:
+        count[layer] = dict()
+        for img_idx in data[layer]:
+            for unit, val in data[layer][img_idx][:top_n]:
+                if unit in count[layer]:
+                    count[layer][unit] += 1
                 else:
-                    count[n][key] = 1
+                    count[layer][unit] = 1
+
     return count
 
 def compare_index_single_layer(data1, data2, order, top_n, image_names):
@@ -228,24 +227,6 @@ def compare_index_arrays(data1, data2, order, top_n):
                 values2.append(vals2[i])
 
     return nr_equals, equals, values1, values2
-#def compare_index_arrays(idxs1, idxs2, order, top_n):
-#    """ 
-#    returns 
-#     1.) the number of equal elements in the first top_n entries of arr1 and arr2
-#     2.) array of equal indices
-#    """
-#    assert len(idxs1) == len(idxs2)
-#    nr_equals = 0
-#    equals = []
-#    if not order:
-#        idxs1 = sorted(idxs1[:top_n])
-#        idxs2 = sorted(idxs2[:top_n])
-#    for i in xrange(top_n):
-#        if idxs1[i] == idxs2[i]:
-#            nr_equals += 1
-#            equals.append(idxs1[i])
-#
-#    return nr_equals, equals
 
 def check_if_contains_duplicates(arr):
     return len(arr) != len(set(arr))
