3a8d0be7dcce572ddf13d17d140a58cdc20522c6
Martin <lautenbacher.martin@gmail.com>
2018-02-12 22:44:19 +0100

    config

--- model_settings/settings_vgg.py
+++ model_settings/settings_vgg.py
@@ -14,13 +14,13 @@ caffevis_labels          = base_folder + './models/vgg/imagenet_labels.txt'
 caffevis_prob_layer      = 'prob'
 
 # Min/max calculation settings
-max_tracker_do_histograms = False
+max_tracker_do_histograms = True
 max_tracker_do_correlation = False
 max_tracker_do_maxes = True
 max_tracker_do_deconv = True
-max_tracker_do_deconv_norm = False
+max_tracker_do_deconv_norm = True
 max_tracker_do_backprop = True
-max_tracker_do_backprop_norm = False
+max_tracker_do_backprop_norm = True
 max_tracker_do_info = True
 
 
--- model_settings/settings_vgg_flickrlogos.py
+++ model_settings/settings_vgg_flickrlogos.py
@@ -14,13 +14,13 @@ caffevis_labels          = base_folder + './models/vgg_flickrlogos/flickrlogos_l
 caffevis_prob_layer      = 'prob'
 
 # Min/max calculation settings
-max_tracker_do_histograms = False
+max_tracker_do_histograms = True
 max_tracker_do_correlation = False
 max_tracker_do_maxes = True
 max_tracker_do_deconv = True
-max_tracker_do_deconv_norm = False
+max_tracker_do_deconv_norm = Frue
 max_tracker_do_backprop = True
-max_tracker_do_backprop_norm = False
+max_tracker_do_backprop_norm = True
 max_tracker_do_info = True
 
 def caffevis_layer_pretty_name_fn(name):
