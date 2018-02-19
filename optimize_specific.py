e1229b722855b964d62514c87a03a153347938a9
Martin <lautenbacher.martin@gmail.com>
2018-02-19 16:54:17 +0100

    done

--- optimize_specific.py
+++ optimize_specific.py
@@ -13,10 +13,10 @@ import settings
 def main():
     parser = argparse.ArgumentParser(description='executes optimize_image.py for specific layers/units')
     parser.add_argument('unit_list',    type = str, default = None, help = 'path to list of units to consider')
+    parser.add_argument('--caffe_root', type = str, default = settings.caffevis_caffe_root, help = 'Path to caffe root directory.')
     parser.add_argument('--nmt_pkl',      type = str, default = os.path.join(settings.caffevis_outputs_dir, 'find_max_acts_output.pickled'), help = 'Which pickled NetMaxTracker to load.')
     parser.add_argument('--net_prototxt', type = str, default = settings.caffevis_deploy_prototxt, help = 'network prototxt to load')
     parser.add_argument('--net_weights',  type = str, default = settings.caffevis_network_weights, help = 'network weights to load')
-    parser.add_argument('--datadir',      type = str, default = settings.static_files_dir, help = 'directory to look for files in')
     parser.add_argument('--filelist',     type = str, default = settings.static_files_input_file, help = 'List of image files to consider, one per line. Must be the same filelist used to produce the NetMaxTracker!')
     parser.add_argument('--outdir',       type = str, default = settings.caffevis_outputs_dir, help = 'Which output directory to use. Files are output into outdir/layer/unit_%%04d/{maxes,deconv,backprop}_%%03d.png')
   
@@ -31,7 +31,10 @@ def main():
     
     for layer in unit_list:
             if not os.path.exists(os.path.join(args.outdir, layer)) and not layer in args.ignore_layers:
-                print layer
+                units = set(unit_list[layer])
+                for unit in units:
+                    os.system("optimize_image.py --caffe-root=%s --deploy-proto=%s --net_weights=%s --data-size=(224, 224) --push-layers=%s --push-channel=%d --output-prefix=%s" % (args.caffe_root, args.net_prototxt, args.net_weights, layer, unit, os.path.join(args.outdir, layer, str(unit), 'opt_' + str(unit))))
+
 
 if __name__ == '__main__':
     main()
